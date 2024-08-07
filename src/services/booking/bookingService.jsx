734707be814fa132298cee0d49506111f6e5f91d
Christian Chukwuebuka <cchukwuebuka@swglobal.com>
2024-08-07 15:05:46 +0100

    fix for id on add aircraft

--- src/services/booking/bookingService.jsx
+++ src/services/booking/bookingService.jsx
@@ -66,6 +66,8 @@ export const AddBookingStep04 = async (bookingId, body) => {
 };
 
 export const GetAvailableAircraftBookingById = async (bookingId) => {
-  const response = await GetRequest(`/available-aircraft?${bookingId}`);
+  const response = await GetRequest(
+    `/available-aircraft?booking_id =${bookingId}`
+  );
   return response;
 };
